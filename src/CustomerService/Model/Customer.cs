53cb236402ee82604afdc9e09723d42445f0dd7c
onkar lonkar <onkarlonkar55@gmail.com>
2026-09-13 21:49:37 +0530

    Add Customer CRUD with PostgreSQL and EF Core

--- src/CustomerService/Model/Customer.cs
+++ src/CustomerService/Model/Customer.cs
@@ -4,7 +4,7 @@ public class Customer
 {
     public int Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
+    public string Name {    get; set; } = string.Empty;
 
     public string Email { get; set; } = string.Empty;
 
